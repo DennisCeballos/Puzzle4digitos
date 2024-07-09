9746065ca289e0b08360935addba85ce4fb71ee9
DennisCeballos <86538156+DennisCeballos@users.noreply.github.com>
2024-07-09 18:02:41 -0500

    Inicio de Algoritmo Dijkstra

--- main.cpp
+++ main.cpp
@@ -6,6 +6,131 @@
 #include <vector>
 #include <cmath>
 
+// 
+// * Algoritmo Dijkstra *
+//  Adaptado GeeksForGeeks
+//
+
+int NO_PARENT = -1;
+
+// Function to print shortest path
+// from source to currentVertex
+// using parents array
+void printPath(int currentVertex, std::vector<int> parents)
+{
+	// Base case : Source node has
+	// been processed
+	if (currentVertex == NO_PARENT) {
+		return;
+	}
+	printPath(parents[currentVertex], parents);
+	std::cout << currentVertex << " ";
+}
+
+// A utility function to print
+// the constructed distances
+// array and shortest paths
+void printSolution(int startVertex, std::vector<int> distances, std::vector<int> parents)
+{
+	int nVertices = distances.size();
+	std::cout << "Vertex\t Distance\tPath";
+
+	for (int vertexIndex = 0; vertexIndex < nVertices; vertexIndex++)
+    {
+		if (vertexIndex != startVertex)
+        {
+			std::cout << "\n" << startVertex << " -> ";
+			std::cout << vertexIndex << " \t\t ";
+			std::cout << distances[vertexIndex] << "\t\t";
+			printPath(vertexIndex, parents);
+		}
+	}
+}
+
+// Function that implements Dijkstra's
+// single source shortest path
+// algorithm for a graph
+void dijkstra(int nVertices, std::vector<std::vector<int>> adjacencyMatrix, int startVertex)
+{
+	// shortestDistances[i] will hold the
+	// shortest distance from src to i
+	std::vector<int> shortestDistances(nVertices);
+
+	// added[i] will true if vertex i is
+	// included / in shortest path tree
+	// or shortest distance from src to
+	// i is finalized
+	std::vector<bool> added(nVertices);
+
+	// Initialize all distances as
+	// INFINITE and added[] as false
+	for (int vertexIndex = 0; vertexIndex < nVertices;
+		vertexIndex++) {
+		shortestDistances[vertexIndex] = INT_MAX;
+		added[vertexIndex] = false;
+	}
+
+	// Distance of source vertex from
+	// itself is always 0
+	shortestDistances[startVertex] = 0;
+
+	// Parent array to store shortest
+	// path tree
+	std::vector<int> parents(nVertices);
+
+	// The starting vertex does not
+	// have a parent
+	parents[startVertex] = NO_PARENT;
+
+	// Find shortest path for all
+	// vertices
+	for (int i = 1; i < nVertices; i++) {
+
+		// Pick the minimum distance vertex
+		// from the set of vertices not yet
+		// processed. nearestVertex is
+		// always equal to startNode in
+		// first iteration.
+		int nearestVertex = -1;
+		int shortestDistance = INT_MAX;
+		for (int vertexIndex = 0; vertexIndex < nVertices;
+			vertexIndex++) {
+			if (!added[vertexIndex]
+				&& shortestDistances[vertexIndex]
+					< shortestDistance) {
+				nearestVertex = vertexIndex;
+				shortestDistance
+					= shortestDistances[vertexIndex];
+			}
+		}
+
+		// Mark the picked vertex as
+		// processed
+		added[nearestVertex] = true;
+
+		// Update dist value of the
+		// adjacent vertices of the
+		// picked vertex.
+		for (int vertexIndex = 0; vertexIndex < nVertices;
+			vertexIndex++) {
+			int edgeDistance
+				= adjacencyMatrix[nearestVertex]
+								[vertexIndex];
+
+			if (edgeDistance > 0
+				&& ((shortestDistance + edgeDistance)
+					< shortestDistances[vertexIndex])) {
+				parents[vertexIndex] = nearestVertex;
+				shortestDistances[vertexIndex]
+					= shortestDistance + edgeDistance;
+			}
+		}
+	}
+
+	printSolution(startVertex, shortestDistances, parents);
+}
+
+
 //Recibe un 'numero' y retorna el digito que se encuentre en la posicion 'pos'
 int digitoEn(int numero, int pos)
 {
